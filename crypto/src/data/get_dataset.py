e0d869f3ffbfdd756d9d9eaf3b4ef2526a3df62d
albeertito7 <albertperezdatsira@gmail.com>
2021-04-07 14:42:44 +0200

    Update get_dataset.py

--- crypto/src/data/get_dataset.py
+++ crypto/src/data/get_dataset.py
@@ -7,25 +7,27 @@ from dotenv import find_dotenv, load_dotenv
 import requests
 import shutil
 import tarfile
-import io
+import os
 
 @click.command()
 @click.argument('output_filepath', type=click.Path(exists=True))
 def main(output_filepath):
     """ 
         Get data from:
-            http://codeandbeer.org/virtual/BigData/Datasets/measures.tgz
+            http://codeandbeer.org/virtual/BigData/Datasets/cryptocurrencypricehistory.tgz   
         into (../data/raw)
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
-    base_url = "http://codeandbeer.org/virtual/BigData/Datasets/measures.tgz"
-    r = requests.get(base_url, stream=True)
-    if r.status_code == 200:
-        opened_tar = tarfile.open(io.BytesIO(r.content))
-        if tarfile.is_tarfile(opened_tar):
-            opened_tar.extractall()
+    base_url = "http://codeandbeer.org/virtual/BigData/Datasets/"
+    filename = "cryptocurrencypricehistory.tgz"
+
+    response = requests.get(base_url+filename, stream=True)
+    if response.status_code == 200:
+        tarfile.open(fileobj=response.raw, mode="r|gz").extractall(output_filepath)
+        #with open(output_filepath+"/"+filename, 'wb') as f:
+            #f.write(response.raw.read())
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
